3a0004709e3760fc12f79487ddd609469bb84866
Johannes Ohlemacher <johannes.ohlemacher@googlemail.com>
2016-07-15 17:25:50 +0200

    fixed missing save of string events

--- neo/d3xp/gamesys/Event.cpp
+++ neo/d3xp/gamesys/Event.cpp
@@ -745,13 +745,13 @@ void idEvent::Shutdown( void ) {
 idEvent::Save
 ================
 */
-void idEvent::Save( idSaveGame *savefile ) {
-	char *str;
+void idEvent::Save( idSaveGame *savefile ) {	
 	int i, size;
 	idEvent	*event;
 	byte *dataPtr;
 	bool validTrace;
 	const char	*format;
+	idStr str;
 
 	savefile->WriteInt( EventQueue.Num() );
 
@@ -780,6 +780,12 @@ void idEvent::Save( idSaveGame *savefile ) {
 					savefile->WriteVec3( *reinterpret_cast<idVec3 *>( dataPtr ) );
 					size += sizeof( idVec3 );
 					break;
+				case D_EVENT_STRING:
+					str.Clear();
+					str.Append( reinterpret_cast<char *>(dataPtr) );
+					savefile->WriteString( str );
+					size += MAX_STRING_LEN;
+					break;
 				case D_EVENT_TRACE :
 					validTrace = *reinterpret_cast<bool *>( dataPtr );
 					savefile->WriteBool( validTrace );
@@ -790,8 +796,8 @@ void idEvent::Save( idSaveGame *savefile ) {
 						SaveTrace( savefile, t );
 						if ( t.c.material ) {
 							size += MAX_STRING_LEN;
-							str = reinterpret_cast<char *>( dataPtr + sizeof( bool ) + sizeof( trace_t ) );
-							savefile->Write( str, MAX_STRING_LEN );
+							const char* s = reinterpret_cast<char *>( dataPtr + sizeof( bool ) + sizeof( trace_t ) );
+							savefile->Write( s, MAX_STRING_LEN );
 						}
 					}
 					break;
